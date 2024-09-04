f9b584d8bcecc8aef5083f00ce88ee2a7fb8b5d2
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-04 12:55:58 +0000

    add test t2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,13 @@
 import sys
 
 if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print(f"{num_args} arguments.")
+    elif num_args == 1:
+        print(f"{num_args} argument:")
+    else:
+        print(f"{num_args} arguments:")
 
-    print(f"{len(sys.argv) - 1} arguments:")
     for i, arg in enumerate(sys.argv[1:], start=1):
         print(f"{i}: {arg}")
