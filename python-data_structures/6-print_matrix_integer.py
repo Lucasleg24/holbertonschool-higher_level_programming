efb1751e2153cc58192e7c36048e13c594002f3b
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-09 09:16:04 +0000

    add test task 6 with join

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join(str(x) for x in row))
+        for i, element in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(element), end=" ")
+            else:
+                print("{:d}".format(element), end="")
+        print()
