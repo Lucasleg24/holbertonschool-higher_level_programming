45c58af8b2842602fe1a2d2ff6326b82a246e594
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-11 12:56:53 +0000

    add comment

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
+
+"""Contains the add_integer fonction"""
+
+
 def add_integer(a, b=98):
-"""
-    Adds two integers or floats after converting them to integers.
 
-    Args:
-        a (int, float): The first number to add. Must be an integer or a float.
-        b (int, float, optional): The second number to add. Defaults to 98.
+    """Add two integer after convert"""
 
-    Returns:
-        int: The sum of the two integers.
-
-    Raises:
-        TypeError: If either a or b is not an integer or float.
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
