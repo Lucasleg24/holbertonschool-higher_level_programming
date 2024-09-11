9eea800f55f7f4f183e8e55d4bd5a018400d1abc
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-11 08:24:36 +0000

    add resolve pycode test

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,8 +8,8 @@ def list_division(my_list_1, my_list_2, list_length):
                 result.append(0)
             elif not isinstance(my_list_1[i], (int, float)) or \
                     not isinstance(my_list_2[i], (int, float)):
-                        print("wrong type")
-                        result.append(0)
+                print("wrong type")
+                result.append(0)
             else:
                 result.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
