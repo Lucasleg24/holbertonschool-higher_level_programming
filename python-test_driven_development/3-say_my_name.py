e6210d8f295b978c50cc87821ca024fe941619a7
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-12 12:32:25 +0000

    add comment

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+
+"""
+Contain th modul with function say_my_name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+
+    """
+    function for print first and last name
+
+    first_name: contain first name
+    last_name: contain last name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
