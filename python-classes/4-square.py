df7b8b2f022b454f8214c5a5c94c306a2d5d6106
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-16 08:45:15 +0000

    add task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+contain the class
+"""
+
+
+class Square:
+
+    """
+    class for print a square
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
