99229d4743b557e4dc367c7121d4b2c618aa44c9
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-16 08:07:52 +0000

    add test t1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+contain the class
+"""
+
+
+class Square:
+
+    """
+    class for print a square
+    """
+
+    def __init__(self, size):
+        self.__size = size
+    pass
