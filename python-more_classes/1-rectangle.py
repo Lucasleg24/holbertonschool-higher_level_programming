d899dbeb6e9ae5a4f42edf36dfb3d6769f0247a8
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-17 09:26:51 +0000

    add test public

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,7 +12,19 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__height = height
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
 
     @property
@@ -26,15 +38,3 @@ class Rectangle:
         elif height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, width):
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
