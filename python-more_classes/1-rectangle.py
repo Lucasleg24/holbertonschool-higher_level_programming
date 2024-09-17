12290462c77de64256076b9654061375616174af
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-17 09:16:03 +0000

    add task 1 tentativ resolve

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,6 +15,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif width < 0:
             raise ValueError("width must be >= 0")
+        self.__width = width
 
     @property
     def height(self):
@@ -22,7 +23,8 @@ class Rectangle:
 
     @height.setter
     def height(self, height):
-        if not isinstance(width, int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         elif height < 0:
             raise ValueError("height must be >= 0")
+        self.__height = height
