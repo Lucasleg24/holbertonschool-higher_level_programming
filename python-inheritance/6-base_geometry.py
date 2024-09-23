5ae398d687885f3e08a951317c5065a70e28634f
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-23 11:10:39 +0000

    add task 6 test

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+contain the class
+"""
+
+
+class BaseGeometry:
+
+    """
+    contain the attribut of the class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
