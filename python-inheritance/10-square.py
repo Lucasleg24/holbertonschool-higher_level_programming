08317aa8f7fc751df01ab10652c3fa7e7b27bb69
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-23 12:31:51 +0000

    add test task 10 with super

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Contain class inherits from rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """
+    Class representing a square, inherits from Rectangle
+    """
+
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+
+        return self.__size ** 2
