372a61d9d22c98fbfe1088841198cf02bf808a9e
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-23 12:21:46 +0000

    add test t8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """
+    Class that represents a Rectangle, inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+
+        """
+        init width and height
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
