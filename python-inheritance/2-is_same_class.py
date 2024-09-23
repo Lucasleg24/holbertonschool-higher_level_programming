3f3c87f3f2d8633b665c670f656bfd6137761f03
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-23 09:15:13 +0000

    add test t2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+contain the class
+"""
+
+
+def is_same_class(obj, a_class):
+
+    """
+    contain the code
+    """
+
+    if type(obj) is not (a_class):
+        return False
+    else:
+        return True
