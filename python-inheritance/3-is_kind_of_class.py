706c591f7bed51604179ec386acb6c4eea59c53f
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-23 09:31:57 +0000

    add test task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+contain the code
+"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """
+    contain the detail
+    """
+
+    if isinstance (obj, a_class):
+        return True
+    else:
+        return False
