33f7ccf27619f558b78d31a3869739b19109a244
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-24 11:22:45 +0000

    add t2

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python3
 
-class VerboseList(list)
+"""
+code
+"""
+
+
+class VerboseList(list):
+
+    """
+    class inherit
+    """
+
+    def append(self, item):
+        """append method"""
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        """extend method"""
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
+
+    def remove(self, item):
+        """remove item"""
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        """pop method"""
+        item = super().pop(index)
+        print("Popped [{}] from the list.".format(item))
+        return item
