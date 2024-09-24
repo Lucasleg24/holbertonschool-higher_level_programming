7d20c07b1cbd95915efec3a677de7c6e6c26ad9b
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-24 09:28:14 +0000

    add task 1 full resovle test abs

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -36,7 +36,7 @@ class Circle(Shape):
         return pi * (self.radius * self.radius)
 
     def perimeter(self):
-        return (2 * self.radius) * pi
+        return abs((2 * self.radius) * pi)
 
 
 class Rectangle(Shape):
