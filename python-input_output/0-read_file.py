11d7651dd78966a800cbbaad226733f5aaa02437
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-30 07:24:30 +0000

    add test t0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
+"""
+contain the def for read a file
+"""
+
+
 def read_file(filename=""):
+
+    """
+    contain the open function
+    """
+
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read(), end="")
