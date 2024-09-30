aa96d4980f061c82d258300fe33723dfef8563e3
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-30 08:23:08 +0000

    add test task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+contain the code of function
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    
+    """
+    contain use json import
+    """
+
+    return json.dumps(my_obj)
