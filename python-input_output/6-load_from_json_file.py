0dd3fdbdd9118d9c912b577f75a5da96709655ab
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-09-30 08:59:23 +0000

    add test t6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+contain the code
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+
+    """
+    contain the json load code
+    """
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.load(file)
