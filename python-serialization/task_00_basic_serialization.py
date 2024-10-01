2c5c84e6ee38a60f01acc6cb0b7ce9a0bbe94713
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-10-01 08:13:26 +0000

    add test t0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
 
-"""
-first task serialization
-"""
-
 import json
+import os
 
 
 def serialize_and_save_to_file(data, filename):
-
+    
     """
-    serialize a data
+    serializes and saves the data
     """
 
-    try:
-        with open(filename, 'w', encoding='utf-8') as file:
-            json.dump(data, file)
-    except Exception as e:
-        print(f"Error during data serialization and saving: {e}")
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    with open(filename, 'w') as file:
+        json_data = json.dumps(data)  # Renommé ici
+        file.write(json_data)
 
 
 def load_and_deserialize(filename):
-
+    
     """
-    deserialize the data
+    load and deserialize the data
     """
 
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        return data
-    except Exception as e:
-        print(f"Error during data loading and deserialization: {e}")
-        return None
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
