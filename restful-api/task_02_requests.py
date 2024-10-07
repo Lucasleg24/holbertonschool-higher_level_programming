38ba6b8980cbf799a217228694971557f0b75eff
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-10-07 13:28:13 +0000

    add test first file

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import request
+import csv
+
+def fetch_and_print_posts():
+
+    """
+    print the post
+    """
+
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f'Status Code: {response.status_code}')
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+        else:
+            print("échec de la reqête")
+
+
+def fetch_and_save_posts():
+
+    """
+    save the post json
+    """
+
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if response.status_code == 200:
+        posts = response.json()
+        posts_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(posts_data)
+
+    else:
+        print("échec de la requê")
