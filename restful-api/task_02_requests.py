104b3ca81c4ac39c1ae8f1264a73c55015570238
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-10-07 13:37:07 +0000

    add solve pycodestyle

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
+"""
+function for print and save data with get method
+"""
+
+
 import requests
 import csv
+
 
 def fetch_and_print_posts():
 
@@ -29,7 +35,14 @@ def fetch_and_save_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
     if response.status_code == 200:
         posts = response.json()
-        posts_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        posts_data = [
+                {
+                    'id': post['id'],
+                    'title': post['title'],
+                    'body': post['body']
+                }
+                for post in posts
+            ]
 
         with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
             writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
