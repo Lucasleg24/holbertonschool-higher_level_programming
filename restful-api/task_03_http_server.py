78a3c358b0e1c74291b5b6d7277c51856313adbb
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-10-09 08:37:55 +0000

    add test with / and info gestion

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,12 +15,25 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     """
 
     def do_GET(self):
-        if self.path == "/data":
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            json_data = {"version": "1.0",
+                         "description": "A simple API built with http.server"}
+            json_bytes = json.dumps(json_data).encode("utf-8")
+            self.wfile.write(json_bytes)
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
