f0c74033a3c0925385eb4253414f95186ded4385
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-10-28 13:54:17 +0100

    add test with show function

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,3 @@
 -- 0 list privileges of user 1 and 2
-SELECT * FROM information_schema.user_privileges
-WHERE grantee LIKE "'user_0d_1'@'localhost'";
-
-SELECT * FROM information_schema.user_privileges
-WHERE grantee LIKE "'user_0d_2'@'localhost'";
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
