83531ed1420fabe036ec9644903138497033ace1
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-10-28 10:47:39 +0100

    add t15 and t16

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- 15 list number records
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- 16 no link
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
