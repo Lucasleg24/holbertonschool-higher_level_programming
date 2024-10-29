e7f79a1e83e7da4f3985922abdbec028ae1aff93
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-10-29 09:41:20 +0100

    add test resolve t8 last check

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,5 @@
 
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.state_id = (
-    SELECT id FROM states WHERE name = 'California'
-)
+WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
