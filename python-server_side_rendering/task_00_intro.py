21588bed311e2e8cf0c4970fe7331bae86c3cd69
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-11-07 14:04:12 +0100

    addt0

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -18,3 +18,37 @@ def generate_invitations(template, attendees):
   if not attendees:
         print("No data provided, no output files generated.")
         return
+
+  for index, attendee in enumerate(attendees, start=1):
+        try:
+            # crée une inviation avec le template "template"
+            invitation = template
+            # Remplace {name} dans invitation
+            # par la valeur de la clé obtenue via get dans attendee
+            invitation = invitation.replace(
+                "{name}", attendee.get("name") or "N/A")
+            invitation = invitation.replace(
+                "{event_title}", attendee.get("event_title") or "N/A")
+            invitation = invitation.replace(
+                "{event_date}", attendee.get("event_date") or "N/A")
+            invitation = invitation.replace(
+                "{event_location}", attendee.get("event_location") or "N/A")
+
+            # Crée le nom du fichier de sortie
+            output_filename = f"output_{index}.txt"
+
+            # Vérifie l'existence du fichier
+            if os.path.exists(output_filename):
+                print(f"The file '{output_filename}' already exist, "
+                      "it will not be overwritten")
+                continue
+
+            # écrit dans le fichier
+            with open(output_filename, "w") as file:
+                file.write(invitation)
+                print(f"Invitation successfully written to "
+                      f"'{output_filename}'.")
+
+        except Exception as e:
+            print(f"Error: An error occurred while generating "
+                  f"'{output_filename}': {e}")
